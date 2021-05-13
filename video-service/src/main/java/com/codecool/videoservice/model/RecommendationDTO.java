383fc32145f4573a5153b0ee9cc2f15cdc4c499a
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 15:05:58 +0200

    fix: modify recommendation.url in application.properties of video-service.

--- video-service/src/main/java/com/codecool/videoservice/model/RecommendationDTO.java
+++ video-service/src/main/java/com/codecool/videoservice/model/RecommendationDTO.java
@@ -1,6 +1,8 @@
 package com.codecool.videoservice.model;
 
 public class RecommendationDTO {
+    private long id;
     private int rating;
     private String comment;
+    private long videoId;
 }
