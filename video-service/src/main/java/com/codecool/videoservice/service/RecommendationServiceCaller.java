0423a0389131ae68c77715d5b89b9b0e696dfbd0
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 14:00:37 +0200

    feat: add constructor to RecommendationServiceCaller. Implement getRecommendationsForVideo method.

--- video-service/src/main/java/com/codecool/videoservice/service/RecommendationServiceCaller.java
+++ video-service/src/main/java/com/codecool/videoservice/service/RecommendationServiceCaller.java
@@ -1,9 +1,14 @@
 package com.codecool.videoservice.service;
 
+import com.codecool.videoservice.model.RecommendationDTO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -14,6 +19,12 @@ public class RecommendationServiceCaller {
     @Value("${recommendation.url}")
     private String baseUrl;
 
+    @Autowired
+    public RecommendationServiceCaller(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
-
+    public List<RecommendationDTO> getRecommendationsForVideo(long videoId){
+        return Arrays.asList(restTemplate.getForEntity(baseUrl + "/" + videoId, RecommendationDTO[].class).getBody());
+    }
 }
