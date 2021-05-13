48ccc3a0e9ed6aaec2e030266a1303349e3bb672
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 14:45:50 +0200

    feat: add findAllByVideoId method to RecommendationRepository.

--- video-recommendation-service/src/main/java/com/codecool/videorecommendationservice/repository/RecommendationRepository.java
+++ video-recommendation-service/src/main/java/com/codecool/videorecommendationservice/repository/RecommendationRepository.java
@@ -3,5 +3,10 @@ package com.codecool.videorecommendationservice.repository;
 import com.codecool.videorecommendationservice.model.Recommendation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
+
+    List<Recommendation> findAllByVideoId(long videoId);
+
 }
