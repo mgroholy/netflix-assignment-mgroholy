7655d8aaee910618ca32e74520cc54f993526374
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 13:39:52 +0200

    feat: add getVideo method to handle GET requests on /video/id endpoint in VideoController.

--- video-service/src/main/java/com/codecool/videoservice/controller/VideoController.java
+++ video-service/src/main/java/com/codecool/videoservice/controller/VideoController.java
@@ -4,6 +4,7 @@ import com.codecool.videoservice.model.Video;
 import com.codecool.videoservice.service.VideoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,6 +22,11 @@ public class VideoController {
     @GetMapping(path="/videos")
     public List<Video> getVideos(){
         return videoService.findAll();
+    }
+
+    @GetMapping(path="/video/{id}")
+    public Video getVideo(@PathVariable(name = "id") int id){
+        return videoService.getVideoWithRecommendations(id);
     }
 
 }
