d9cbc5d6b461119025a47e6e7bac80346b1c0cfb
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 15:08:50 +0200

    feat: add one hardcoded recommendation for video 1 in VideoRecommendationServiceApplication.

--- video-recommendation-service/src/main/java/com/codecool/videorecommendationservice/VideoRecommendationServiceApplication.java
+++ video-recommendation-service/src/main/java/com/codecool/videorecommendationservice/VideoRecommendationServiceApplication.java
@@ -1,13 +1,35 @@
 package com.codecool.videorecommendationservice;
 
+import com.codecool.videorecommendationservice.model.Recommendation;
+import com.codecool.videorecommendationservice.repository.RecommendationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VideoRecommendationServiceApplication {
+
+	private RecommendationRepository recommendationRepository;
+
+	@Autowired
+	public VideoRecommendationServiceApplication(RecommendationRepository recommendationRepository) {
+		this.recommendationRepository = recommendationRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VideoRecommendationServiceApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init(){
+		return args -> {
+			Recommendation video1Recommendation = Recommendation.builder().comment("Great video!").rating(5).videoId(1).build();
+			recommendationRepository.save(video1Recommendation);
+		};
 	}
 
 }
