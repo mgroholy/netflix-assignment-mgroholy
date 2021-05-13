8c15c3b6a757f4ee05c82f967a93c7cbd3dffc6b
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 15:07:31 +0200

    fix: add lombok annotations to RecommendationDTO.

--- video-service/src/main/java/com/codecool/videoservice/model/RecommendationDTO.java
+++ video-service/src/main/java/com/codecool/videoservice/model/RecommendationDTO.java
@@ -1,8 +1,15 @@
 package com.codecool.videoservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class RecommendationDTO {
-    private long id;
     private int rating;
     private String comment;
-    private long videoId;
 }
