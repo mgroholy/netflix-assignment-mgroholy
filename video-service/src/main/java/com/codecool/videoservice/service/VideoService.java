075a8e9f70bd6a5f48aa9e614e4b00adf82bdc38
Máté Groholy <mate.groholy@gmail.com>
2021-05-13 13:02:56 +0200

    feat: create VideoService, implement findAll method.

---
+++ video-service/src/main/java/com/codecool/videoservice/service/VideoService.java
@@ -0,0 +1,23 @@
+package com.codecool.videoservice.service;
+
+import com.codecool.videoservice.model.Video;
+import com.codecool.videoservice.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VideoService {
+
+    private VideoRepository videoRepository;
+
+    @Autowired
+    public VideoService(VideoRepository videoRepository) {
+        this.videoRepository = videoRepository;
+    }
+
+    public List<Video> findAll(){
+        return videoRepository.findAll();
+    }
+}
