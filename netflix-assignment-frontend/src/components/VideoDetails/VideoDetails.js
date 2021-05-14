0ac0f834c8de51a1e7deb68279f3ed09a01719cd
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 11:08:29 +0200

    feat: fetch video in VideoDetails.

--- netflix-assignment-frontend/src/components/VideoDetails/VideoDetails.js
+++ netflix-assignment-frontend/src/components/VideoDetails/VideoDetails.js
@@ -1,16 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { VideoListContainer } from "../VideoList/VideoListElements";
 import { VideoDetailCard } from "./VideoDetailElements";
+import { VIDEOS_API_URL } from "../VideoList/VideoList";
+import ReactPlayer from "react-player";
 
 const VideoDetails = () => {
   const { id } = useParams();
 
+  const [video, setVideo] = useState({});
+
   console.log(id);
+
+  const fetchVideo = async () => {
+    let url = `${VIDEOS_API_URL}/${id}`;
+    const response = await axios.get(url);
+    setVideo(response.data);
+  };
+
+  useEffect(() => {
+    fetchVideo();
+  }, []);
+
   return (
     <VideoListContainer>
       <VideoDetailCard>
-        <p>Video details.</p>
+        <ReactPlayer url={video.url} />
+        <p style={{ color: "white" }}>{video.name}</p>
       </VideoDetailCard>
     </VideoListContainer>
   );
