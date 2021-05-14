8d14eed9889930d551f5dc9aecc40c1e7b57ce34
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 10:41:43 +0200

    feat: create Header component.

---
+++ netflix-assignment-frontend/src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderBar, MainIcon } from "./HeaderElements";
+
+const Header = () => {
+  return (
+    <HeaderBar>
+      <MainIcon size={50} />
+    </HeaderBar>
+  );
+};
+
+export default Header;
