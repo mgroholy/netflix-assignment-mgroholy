6577666b8be9963bfe12eb06a03714a770e97139
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 10:43:14 +0200

    feat: modify VideoListElements style.

--- netflix-assignment-frontend/src/components/VideoList/VideoListElements.js
+++ netflix-assignment-frontend/src/components/VideoList/VideoListElements.js
@@ -2,21 +2,24 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 export const VideoListContainer = styled.div`
-  height: 100%;
+  height: 90vh;
   width: 100%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
 `;
 
 export const VideoListCard = styled.div`
-  height: 50%;
   width: 50%;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: left;
   align-items: center;
+  background-color: #000;
+  margin: 50px;
+  border-radius: 3px;
+  padding: 20px;
 `;
 
 export const VideoListItem = styled(Link)`
