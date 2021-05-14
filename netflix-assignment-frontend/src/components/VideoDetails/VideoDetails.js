c8b6e211155ed8d97ca07e24755e62cfed4fdb2d
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 11:21:38 +0200

    feat: use react-youtube in VideoDetails.

--- netflix-assignment-frontend/src/components/VideoDetails/VideoDetails.js
+++ netflix-assignment-frontend/src/components/VideoDetails/VideoDetails.js
@@ -4,12 +4,13 @@ import { useParams } from "react-router";
 import { VideoListContainer } from "../VideoList/VideoListElements";
 import { VideoDetailCard } from "./VideoDetailElements";
 import { VIDEOS_API_URL } from "../VideoList/VideoList";
-import ReactPlayer from "react-player";
+import YouTube from "react-youtube";
 
 const VideoDetails = () => {
   const { id } = useParams();
 
   const [video, setVideo] = useState({});
+  const [videoId, setVideoId] = useState("");
 
   console.log(id);
 
@@ -17,6 +18,13 @@ const VideoDetails = () => {
     let url = `${VIDEOS_API_URL}/${id}`;
     const response = await axios.get(url);
     setVideo(response.data);
+    getVideoId(response.data.url);
+  };
+
+  const getVideoId = (url) => {
+    const urlParts = url.split("?v=");
+    const id = urlParts[urlParts.length - 1];
+    setVideoId(id);
   };
 
   useEffect(() => {
@@ -26,8 +34,8 @@ const VideoDetails = () => {
   return (
     <VideoListContainer>
       <VideoDetailCard>
-        <ReactPlayer url={video.url} />
         <p style={{ color: "white" }}>{video.name}</p>
+        <YouTube videoId={videoId} />
       </VideoDetailCard>
     </VideoListContainer>
   );
