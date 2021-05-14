a30d8c1abf1aae2f99b2ae2614bb4d6a08f8b260
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 10:11:40 +0200

    feat: fetch videos from backend, display it on card in VideoList.js.

--- netflix-assignment-frontend/src/VideoList/VideoList.js
+++ netflix-assignment-frontend/src/VideoList/VideoList.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   VideoListCard,
   VideoListContainer,
@@ -6,20 +7,33 @@ import {
 } from "./VideoListElements";
 
 const VideoList = () => {
+  const VIDEOS_API_URL = "http://localhost:8762/videos";
+
+  const [videos, setVideos] = useState([]);
+
+  const fetchVideos = async () => {
+    try {
+      const response = await axios.get(VIDEOS_API_URL);
+      setVideos(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
   return (
     <div>
       <VideoListContainer>
         <VideoListCard>
-          <VideoListItem>
-            <p>id</p>
-            <p>Item</p>
-          </VideoListItem>
-          <VideoListItem>
-            <p>Item</p>
-          </VideoListItem>
-          <VideoListItem>
-            <p>Item</p>
-          </VideoListItem>
+          {videos.map((video) => (
+            <VideoListItem to={`/videos/${video.id}`}>
+              <p>{video.id}</p>
+              <p>{video.name}</p>
+            </VideoListItem>
+          ))}
         </VideoListCard>
       </VideoListContainer>
     </div>
