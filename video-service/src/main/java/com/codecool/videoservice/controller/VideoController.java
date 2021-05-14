95f56cc006bcc88f603421b7ea2fcc1a6f8b28ab
Máté Groholy <mate.groholy@gmail.com>
2021-05-14 09:55:48 +0200

    fix: modify endpoint urls in VideoController. /video -> /videos.

--- video-service/src/main/java/com/codecool/videoservice/controller/VideoController.java
+++ video-service/src/main/java/com/codecool/videoservice/controller/VideoController.java
@@ -23,19 +23,19 @@ public class VideoController {
         return videoService.findAll();
     }
 
-    @GetMapping(path="/video/{id}")
+    @GetMapping(path="/videos/{id}")
     public Video getVideo(@PathVariable(name = "id") long id){
         return videoService.getVideoWithRecommendations(id);
     }
 
-    @PostMapping(path="/video/{id}")
+    @PostMapping(path="/videos/{id}")
     public Video updateVideo(@PathVariable(name = "id") long id, @RequestBody Video video){
         video.setId(id);
         videoService.updateVideo(video);
         return video;
 
     }
-    @PostMapping(path="/video/{id}/recommendation")
+    @PostMapping(path="/videos/{id}/recommendation")
     public void addRecommendation(@PathVariable(name="id") long id, @RequestBody RecommendationDTO recommendationDTO){
         recommendationDTO.setVideoId(id);
         videoService.addRecommendation(recommendationDTO);
